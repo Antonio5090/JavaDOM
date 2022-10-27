ce71cd05340f3d7194144774ae9d571e91f9e44c
José Antonio Aguilera Sánchez <lx00707@innovaccion.mx>
2022-10-26 22:56:01 -0500

    Codigo para agregar tareas completado

--- app.js
+++ app.js
@@ -6,13 +6,20 @@ const createTask = (evento) => {
     const value = input.value;
     input.value = "";
     const list = document.querySelector('[data-list]');
+    //data-list{
     const task = document.createElement('li');
+    task.classList.add('card');
+    //backticks
     const content = `<div>
     <i class="far fa-check-square icon"></i>
     <span class="task">${value}</span>
     </div>
     <i class="fas fa-trash-alt trashIcon icon"></i>`
+    //data-list}
     task.innerHTML += content;
+
+    list.appendChild(task);
+
     console.log(content);
 }
 
